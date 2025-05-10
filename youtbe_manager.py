ebc15734395015e3805588f5c636eff244effb67
wahid5183 <aw5650359@gmail.com>
2025-05-10 11:29:40 +0600

    version-2

--- youtbe_manager.py
+++ youtbe_manager.py
@@ -1,73 +1,67 @@
-import json
-def get_videos():
-    try:
-        with open('data.txt','r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return []
-    
+import sqlite3
 
-def save_all_videos(video):
-    with open('data.txt','w') as file:
-        json.dump(video,file)
+conn = sqlite3.connect('youtbe_manger.db')
+cursor = conn.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS videos(
+               id INTEGER PRIMARY KEY,
+               name TEXT NOT NULL,
+               time TEXT NOT NULL,
+               url TEXT NOT NULL
+               )
+""")    
         
 
-def list_videos(videos):
+def list_videos():
     print('\n')
     print('*' * 70)
-    for index,video in enumerate(videos,start=1):
-        print(f"{index}. {video['name']}, Duraion: {video['time']}")
+    cursor.execute("SELECT * FROM videos")
+    for video in cursor:
+        print(f"{video}")
     print('\n')
     print('*' * 70)
 
-def add_video(videos):
-    name = input("Enter your video name: ")
-    time = input("Enter your video time: ")
-    videos.append({'name': name, 'time': time})
-    save_all_videos(videos)
+def add_video(name,time,url):
+    cursor.execute("INSERT INTO videos (name,time,url) VALUES (?,?,?)",(name,time,url))
+    conn.commit()
+    
 
+def update_video(Video_Id,name,time,url):
+    cursor.execute("UPDATE videos SET name = ?, time = ? , url = ? WHERE id = ?",(name,time,url,Video_Id))
+    conn.commit()
 
-
-def update_video(videos):
-    list_videos(videos)
-    index =int(input("Enter the Video Index to update"))
-    if 1 <= index <= len(videos):
-        name = input("Enter New Video Name: ")
-        duration = input("Enter New Video Duration: ")
-        videos[index-1] = {'name': name, 'time': duration}
-        save_all_videos(videos)
-    else:
-        print("Invalid Input")
-
-def delete_video(videos):
-    list_videos(videos)
-    index =int(input("Enter the Video Index to delete"))
-    if 1 <= index <= len(videos):
-        del videos[index-1]
-        save_all_videos(videos)
-    else:
-        print("Invalid Input")
+def delete_video(video_Id):
+    cursor.execute("DELETE FROM videos WHERE id = ?",(video_Id,))
 
     
 def main():
     while True:
-        videos = get_videos()
         print("1. All Videos List")
         print("2. Add a Video")
         print("3. Update a video")
         print("4. Delete a video")
         chioce = input("Give your chioce : ")
-        match chioce:
-            case '1':
-                list_videos(videos)
-            case '2':
-                add_video(videos)
-            case '3':
-                update_video(videos)
-            case '4':
-                delete_video(videos)
-            case _:
-                return
+        if chioce == '1':
+            list_videos()
+        elif chioce == '2':
+            name = input("Enter the Video name : ")
+            time = input("Enter the Video time : ")
+            url = input("Enter the Video URL : ")
+            add_video(name, time, url)
+        elif chioce == '3':
+            list_videos()
+            video_Id = input("Enter the VIDEO ID to Update : ")
+            name = input("Enter the Video name : ")
+            time = input("Enter the Video time : ")
+            url = input("Enter the Video URL : ")
+            update_video(video_Id,name, time, url)
+        elif chioce == '4':
+            list_videos()
+            video_Id = input("Enter the VIDEO ID to Delete : ")
+            delete_video(video_Id)
+        else: 
+            print("Invalid Input")
+    conn.close()
+
             
 if __name__ == '__main__': main()
         
